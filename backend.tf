3421e0d4351f796b7ff069fa0f0c757c0e4d88ab
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-19 12:01:52 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "s3-backend-for-testap"
+    key =    "network/test.tfstate"
+    region = "us-east-1"
+    #encrypt = true
+    #dynamodb_table = "test-dynamodb-table"
+    }
+}
