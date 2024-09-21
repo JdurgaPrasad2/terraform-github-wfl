e119605044096de4ab5dfa28f253833b4c5933be
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-21 22:42:42 +0530

    Update main.tf

--- modules/batch/main.tf
+++ modules/batch/main.tf
@@ -1,6 +1,13 @@
+
+locals {
+  compute_env_name      = "${var.project}_${var.compute_env_name}_${var.env}"
+  job_queue_name        = "${var.project}_${var.job_queue_name}_${var.env}"
+  job_def_name          = "${var.project}_${var.job_def_name}_${var.env}"
+}
+
 ## IAM Role and Policies for Batch compute environment 
 
-data "aws_iam_policy_document" "batch_assume_role" {
+data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
 
@@ -13,20 +20,20 @@ data "aws_iam_policy_document" "batch_assume_role" {
   }
 }
 
-resource "aws_iam_role" "aws_batch_service_role" {
-  name               = "aws_batch_service_role"
-  assume_role_policy = data.aws_iam_policy_document.batch_assume_role.json
+resource "aws_iam_role" "service_role" {
+  name               = "${var.project}_batch_service_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-resource "aws_iam_role_policy_attachment" "aws_batch_service_role" {
-  role       = aws_iam_role.aws_batch_service_role.name
+resource "aws_iam_role_policy_attachment" "service_role" {
+  role       = aws_iam_role.service_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
 }
 
 # Security group for batch compute environment 
 
-resource "aws_security_group" "sample" {
-  name = "aws_batch_compute_environment_security_group"
+resource "aws_security_group" "compute_environment" {
+  name = "${var.project}_batch_compute_env_${var.env}"
 
   egress {
     from_port   = 0
@@ -38,37 +45,35 @@ resource "aws_security_group" "sample" {
 
 ## Batch Compute environment
 
-resource "aws_batch_compute_environment" "sample" {
-  compute_environment_name = "sample"
+resource "aws_batch_compute_environment" "compute_environment" {
+  compute_environment_name = local.compute_env_name
 
   compute_resources {
     max_vcpus = 4
 
     security_group_ids = [
-      aws_security_group.sample.id
+      aws_security_group.compute_environment.id
     ]
 
-    subnets = [
-      aws_subnet.sample.id
-    ]
+    subnets = var.subnet_ids
 
-    type = "FARGATE"
+    type = var.compute_resource_type
   }
 
-  service_role = aws_iam_role.aws_batch_service_role.arn
-  type         = "MANAGED"
-  depends_on   = [aws_iam_role_policy_attachment.aws_batch_service_role]
+  service_role = aws_iam_role.service_role.arn
+  type         = var.compute_env_type
+  #depends_on   = [aws_iam_role_policy_attachment.service_role]
 }
 
 #Batch Job Queue
 
-resource "aws_batch_job_queue" "test_queue" {
-  name     = "tf-test-batch-job-queue"
-  state    = "ENABLED"
+resource "aws_batch_job_queue" "job_queue" {
+  name     = local.job_queue_name
+  state    = var.job_queue_state
   priority = 1
 
   compute_environment_order {
     order               = 1
-    compute_environment = aws_batch_compute_environment.test_environment_1.arn
+    compute_environment = aws_batch_compute_environment.compute_environment.arn
   }
 }
