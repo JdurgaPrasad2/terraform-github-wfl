731234411ad8e522a622c378f0169958507e3f52
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-22 14:51:32 +0530

    Update outputs.tf

--- modules/batch/outputs.tf
+++ modules/batch/outputs.tf
@@ -1,5 +1,5 @@
 # outputs.tf
-/*
+
 output "batch_compute_environment_arn" {
   description = "Generated batch compute environment arn"
   value       = aws_batch_compute_environment.compute_environment.arn
@@ -9,7 +9,7 @@ output "batch_job_queue_arn" {
   description = "Generated batch job queue arn"
   value       = aws_batch_job_queue.job_queue.arn
 }
-
+/*
 output "batch_job_definition_arn" {
   description = "Generated batch job definition arn"
   value       = aws_batch_job_definition.job_definition.arn
