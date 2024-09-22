ff79b46d804c91810553142a79a861d876b981e5
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-22 13:43:57 +0530

    Update main.tf

--- modules/batch/main.tf
+++ modules/batch/main.tf
@@ -5,31 +5,6 @@ locals {
   job_def_name          = "${var.project}_${var.job_def_name}_${var.env}"
 }
 
-## iam role and policies for batch compute environment 
-
-data "aws_iam_policy_document" "batch_service_assume" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["batch.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "batch_service" {
-  name               = "${var.project}_batch_service_role"
-  assume_role_policy = data.aws_iam_policy_document.batch_service_assume.json
-}
-
-resource "aws_iam_role_policy_attachment" "batch_service" {
-  role       = aws_iam_role.batch_service.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
-}
-
 # security group for batch compute environment 
 
 resource "aws_security_group" "compute_environment" {
@@ -76,34 +51,6 @@ resource "aws_batch_job_queue" "job_queue" {
     order               = 1
     compute_environment = aws_batch_compute_environment.compute_environment.arn
   }
-}
-
-## iam role and policies for batch job definition 
-
-data "aws_iam_policy_document" "batch_job_exec_assume" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "batch_job_exec" {
-  name               = "${var.project}_batch_job_exec_assume_role"
-  assume_role_policy = data.aws_iam_policy_document.batch_job_exec_assume.json
-}
-
-resource "aws_iam_role_policy_attachment" "batch_job_exec" {
-  for_each = toset( [ "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess",
-                      "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
-                      "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-                    ])  
-  role       =  aws_iam_role.batch_job_exec.name
-  policy_arn = each.key
 }
 
 #batch job definition
