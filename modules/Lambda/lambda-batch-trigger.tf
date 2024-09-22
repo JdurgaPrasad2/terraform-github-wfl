fb4593ad2ae6e1648b222b3a9c36ec622f4a471c
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-22 21:15:10 +0530

    Update lambda-batch-trigger.tf

--- modules/Lambda/lambda-batch-trigger.tf
+++ modules/Lambda/lambda-batch-trigger.tf
@@ -1,70 +1,13 @@
 #create batch trigge source code zip 
+
 data "archive_file" "batch_trigger_source_code" {
   type        = "zip"
   source_dir  = "./batch-trigger-source"
   output_path = "./batch-trigger-source"
 } 
 
-#lambda service assume policy
-data "aws_iam_policy_document" "lambda_service_assume" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      identifiers = ["lambda.amazonaws.com"]
-      type        = "Service"
-    }
-  }
-}
-
-#lambda service role  
-resource "aws_iam_role" "lambda_execution" {
-  name               = "${var.project}-lambda-execution-role"
-  assume_role_policy = data.aws_iam_policy_document.lambda_service_assume.json
-}
-
-#lambda execution policy
-data "aws_iam_policy_document" "lambda_execution" {
-  statement {
-    actions = [
-      "logs:CreateLogGroup",
-    ]
-
-    resources = ["arn:aws:logs:${var.region}:311324824904:*"]
-  }
-
-  statement {
-    actions = [
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-    ]
-
-    resources = ["arn:aws:logs:${var.region}:311324824904:log-group:/aws/lambda/${aws_lambda_function.batch_trigger.id}:*"]
-  }
-
-  statement {
-    actions = [
-      "lambda:InvokeFunction"
-    ]
-
-    resources = ["*"]
-  }
-
-}
-
-resource "aws_iam_role_policy" "lambda_execution" {
-  name   = "${var.project}-lambda-execution-policy"
-  policy = data.aws_iam_policy_document.lambda_execution.json
-  role   = aws_iam_role.lambda_execution.id
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_execution" {
-  for_each = toset( [ "arn:aws:iam::aws:policy/AWSBatchFullAccess" ] )  
-  role       =  aws_iam_role.lambda_execution.name
-  policy_arn = each.key
-}
-
 # batch trigger lambda function 
+
 resource "aws_lambda_function" "batch_trigger" {
     function_name = "sagerx-${var.department}-${var.env}-low-util-workspaces"
     filename = data.archive_file.batch_trigger_source_code.output_path
@@ -80,6 +23,7 @@ resource "aws_lambda_function" "batch_trigger" {
       }
     }
 }
+
 /*
 #create cloudwatch event bridge rule 
 resource "aws_cloudwatch_event_rule" "schedule" {
