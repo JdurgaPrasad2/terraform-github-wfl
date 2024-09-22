bf36c01dd60b769973f3fc7f59eadf308593d373
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-22 15:10:08 +0530

    Update outputs.tf

--- modules/batch/outputs.tf
+++ modules/batch/outputs.tf
@@ -9,9 +9,8 @@ output "batch_job_queue_arn" {
   description = "Generated batch job queue arn"
   value       = aws_batch_job_queue.job_queue.arn
 }
-/*
+
 output "batch_job_definition_arn" {
   description = "Generated batch job definition arn"
   value       = aws_batch_job_definition.job_definition.arn
 }
-*/
