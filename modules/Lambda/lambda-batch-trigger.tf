5481efd564b5670d4e98ea30a58b1fa57bec081d
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-22 23:05:25 +0530

    Update lambda-batch-trigger.tf

--- modules/Lambda/lambda-batch-trigger.tf
+++ modules/Lambda/lambda-batch-trigger.tf
@@ -1,17 +1,17 @@
-#create batch trigge source code zip 
+#lambda source code zip 
 
-data "archive_file" "batch_trigger_source_code" {
+data "archive_file" "lambda_source_code" {
   type        = "zip"
   source_dir  = var.source_dir
   output_path = "${var.output_path}.zip"
 } 
 
-# batch trigger lambda function 
+# lambda function 
 
 resource "aws_lambda_function" "batch_trigger" {
-    function_name = "${var.project}-batch-trigger-${var.env}"
-    filename = data.archive_file.batch_trigger_source_code.output_path
-    source_code_hash = data.archive_file.batch_trigger_source_code.output_base64sha256
+    function_name = "${var.project}-${var.function_name}-${var.env}"
+    filename = data.archive_file.lambda_source_code.output_path
+    source_code_hash = data.archive_file.lambda_source_code.output_base64sha256
     role = aws_iam_role.lambda_execution.arn
     handler = var.handler
     runtime = var.runtime 
