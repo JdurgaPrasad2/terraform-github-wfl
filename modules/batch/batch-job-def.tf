4b10774083e1bdc4f24bc3aae4220bcb7414fe9f
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-22 13:51:43 +0530

    Update and rename main.tf to batch-job-def.tf

--- modules/batch/batch-job-def.tf
+++ modules/batch/batch-job-def.tf
@@ -1,21 +1,6 @@
 
 locals {
-  compute_env_name      = "${var.project}_${var.compute_env_name}_${var.env}"
-  job_queue_name        = "${var.project}_${var.job_queue_name}_${var.env}"
   job_def_name          = "${var.project}_${var.job_def_name}_${var.env}"
-}
-
-#batch job queue
-
-resource "aws_batch_job_queue" "job_queue" {
-  name     = local.job_queue_name
-  state    = var.job_queue_state
-  priority = 1
-
-  compute_environment_order {
-    order               = 1
-    compute_environment = aws_batch_compute_environment.compute_environment.arn
-  }
 }
 
 #batch job definition
