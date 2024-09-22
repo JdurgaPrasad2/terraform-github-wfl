61448cf732e384dcc955db4b7b0de97f278d4ef0
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-23 00:59:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,14 +9,22 @@ module "test-s3" {
 }
 */
 
+locals {
+  compute_env_name      = "${var.project}-${var.compute_env_name}-${var.env}"
+  job_queue_name        = "${var.project}-${var.job_queue_name}-${var.env}"
+  job_def_name          = "${var.project}-${var.job_def_name}-${var.env}"
+  job_name              = "${var.project}-${var.job_name}-${var.env}"
+  function_name         = "${var.project}-${var.batch_trigger_function_name}-${var.env}"
+}
+
 module "batch" {
   source                = "./modules/batch"
   project               = var.project
   region                = var.region
   env                   = var.env
-  compute_env_name      = "${var.project}-${var.compute_env_name}-${var.env}"
-  job_queue_name        = "${var.project}-${var.job_queue_name}-${var.env}"
-  job_def_name          = "${var.project}-${var.job_def_name}-${var.env}"
+  compute_env_name      = local.compute_env_name
+  job_queue_name        = local.job_queue_name
+  job_def_name          = local.job_def_name
   subnet_ids            = var.subnet_ids
   compute_resource_type = var.compute_resource_type
   compute_env_type      = var.compute_env_type
@@ -31,13 +39,13 @@ module "lambda-batch-trigger" {
   project                 = var.project
   region                  = var.region  
   env                     = var.env
-  function_name           = "${var.project}-${var.batch_trigger_function_name}-${var.env}"
+  function_name           = local.function_name
   source_dir              = var.batch_trigger_src_dir
   output_path             = var.batch_trigger_src_op_path
-  job_queue_name          = "${var.project}-${var.job_queue_name}-${var.env}"
-  job_def_name            = "${var.project}-${var.job_def_name}-${var.env}"
+  job_queue_name          = local.job_queue_name
+  job_def_name            = local.job_def_name
   handler                 = "${var.batch_trigger_src_op_path}.lambda_handler"
   runtime                 = var.runtime
-  job_name                = "${var.project}-${var.job_name}-${var.env}"
+  job_name                = local.job_name
   #schedule-expression     = var.schedule-expression
 }
