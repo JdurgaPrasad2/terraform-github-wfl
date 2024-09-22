091540e8e34e136111c1f1588a2a477fb7b6daf7
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-23 02:01:52 +0530

    Update batch-trigger-src.py

--- batch-trigger-src/batch-trigger-src.py
+++ batch-trigger-src/batch-trigger-src.py
@@ -2,8 +2,8 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    print("Hello World")
+    print(str(Square(4)))
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Hello from Lambda!'+ str(Square(4)))
     }
