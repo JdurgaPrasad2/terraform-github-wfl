ff3e27f6890296e8f5092575537602ca76e901a8
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-22 13:50:13 +0530

    Create batch-job-queue.tf

---
+++ modules/batch/batch-job-queue.tf
@@ -0,0 +1,16 @@
+locals {
+  job_queue_name        = "${var.project}_${var.job_queue_name}_${var.env}"
+}
+
+#batch job queue
+
+resource "aws_batch_job_queue" "job_queue" {
+  name     = local.job_queue_name
+  state    = var.job_queue_state
+  priority = 1
+
+  compute_environment_order {
+    order               = 1
+    compute_environment = aws_batch_compute_environment.compute_environment.arn
+  }
+}
