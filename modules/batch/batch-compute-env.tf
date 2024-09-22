58b82a8930cb17ab8f957eb618e392bc9fb1d791
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-22 17:25:52 +0530

    Update batch-compute-env.tf

--- modules/batch/batch-compute-env.tf
+++ modules/batch/batch-compute-env.tf
@@ -36,5 +36,10 @@ resource "aws_batch_compute_environment" "compute_environment" {
 
   service_role = aws_iam_role.batch_service.arn
   type         = var.compute_env_type
-  #depends_on   = [aws_iam_role_policy_attachment.service_role]
+
+  tags = {
+    "Name"                           = "${local.compute_env_name}"
+    "Project"                        = "${var.project}"
+    "Environment"                    = "${var.env}"
+  }
 }
