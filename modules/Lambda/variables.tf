eb0e7ec4633e6582fda3f90ec8911eb840b3142f
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-22 21:29:38 +0530

    Create variables.tf

---
+++ modules/Lambda/variables.tf
@@ -0,0 +1,41 @@
+# variables.tf
+
+variable "env" {
+  description = "Target environment for the deployment"
+  type        = string
+}
+
+variable "region" {
+  description = "Target region for the deployment"
+  type        = string
+}
+
+variable "project" {
+  description = "Department requesting the resource"
+  type        = string
+}
+
+variable "handler" {
+    description = "function handler"
+    type = string
+}
+
+variable "runtime" {
+    description = "runtime"
+    type = string
+}
+
+variable "job_queue_name" {
+  description = "job queue name"
+  type = string
+}
+
+variable "job_def_name" {
+  description = "job definition name"
+  type = string
+}
+
+variable "job_name" {
+  description = "batch job name"
+  type = string
+}
