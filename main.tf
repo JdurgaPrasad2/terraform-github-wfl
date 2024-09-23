73c3852fe9b9d1b280bf148b2a7834e02e4acf82
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-23 20:12:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,9 @@ locals {
   job_def_name          = "${var.project}-${var.job_def_name}-${var.env}"
   job_name              = "${var.project}-${var.job_name}-${var.env}"
   function_name         = "${var.project}-${var.batch_trigger_function_name}-${var.env}"
+  event_rule_name       = "${var.project}-${var.event_rule_name}-${var.env}"
+  event_rule_desc       = ""
+  event_schedule        = "rate(10 min)"
 }
 
 module "batch" {
@@ -32,6 +35,22 @@ module "batch" {
   ecr_app_code_image    = var.ecr_app_code_image
   assign_public_ip      = var.assign_public_ip
   job_def_type          = var.job_def_type
+}
+
+module "lambda-batch-trigger" {
+  source                  = "./modules/lambda"
+  project                 = var.project
+  region                  = var.region  
+  env                     = var.env
+  function_name           = local.function_name
+  source_dir              = var.batch_trigger_src_dir
+  output_path             = var.batch_trigger_src_op_path
+  job_queue_name          = local.job_queue_name
+  job_def_name            = local.job_def_name
+  handler                 = "${var.batch_trigger_src_op_path}.lambda_handler"
+  runtime                 = var.runtime
+  job_name                = local.job_name
+  #schedule-expression     = var.schedule-expression
 }
 
 module "lambda-batch-trigger" {
