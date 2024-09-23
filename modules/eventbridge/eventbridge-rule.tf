c06cc8b8cba3938b9f468028f8502f2f5bb5c6c3
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-23 19:34:07 +0530

    Update eventbridge-rule.tf

--- modules/eventbridge/eventbridge-rule.tf
+++ modules/eventbridge/eventbridge-rule.tf
@@ -1,1 +1,23 @@
 
+#event bridge-event rule 
+resource "aws_cloudwatch_event_rule" "event_rule" {
+    name                = var.event_rule_name
+    description         = var.event_rule_desc
+    schedule_expression = var.event_schedule
+} 
+
+#event bridge rule-event target
+resource "aws_cloudwatch_event_target" "event_target" {
+    rule      = aws_cloudwatch_event_rule.event_rule.name
+    target_id = var.function_name
+    arn       = aws_lambda_function.function.arn
+}
+
+#lambda invoke permission to event rule
+resource "aws_lambda_permission" "event_rule_lambda" {
+    statement_id   = "AllowExecutionFromCloudWatch"
+    action         = "lambda:InvokeFunction"
+    function_name  = var.function_name
+    principal      = "events.amazonaws.com"
+    source_arn     = aws_cloudwatch_event_rule.event_rule.arn
+}
