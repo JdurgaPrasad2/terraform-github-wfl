5ed39364fe20a161f5dc54f0c61f53db222996cb
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-23 18:57:29 +0530

    Update batch-iam.tf

--- modules/batch/batch-iam.tf
+++ modules/batch/batch-iam.tf
@@ -38,7 +38,7 @@ data "aws_iam_policy_document" "batch_job_exec_assume" {
 }
 
 resource "aws_iam_role" "batch_job_exec" {
-  name               = "${var.project}-batch-job-exec-assume-role"
+  name               = "${var.project}-batch-job-exec-role"
   assume_role_policy = data.aws_iam_policy_document.batch_job_exec_assume.json
 }
 
