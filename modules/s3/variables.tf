5cbe1d91b74f37d9c295942d86c8935e0adb59c1
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-24 11:14:23 +0530

    Update variables.tf

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,4 +1,5 @@
 # variables.tf
+
 variable "env" {
   description = "Target environment for the deployment"
   type        = string
@@ -9,11 +10,49 @@ variable "region" {
   type        = string
 }
 
-variable "department" {
+variable "project" {
   description = "Department requesting the resource"
   type        = string
+  default     = "doc"
 }
 
+variable "bucket_name" {
+    description = "s3 bucket name"
+    type = string
+}
+
+variable "sqs_queue_arn" {
+    description = "sqs queue arn"
+    type = string
+    default = null
+}
+
+variable "s3_events" {
+  description = "list of s3 events"
+  type = list(string)
+  default = []
+}
+
+variable "filter_suffix" {
+  description = "s3 bucketnotification filter suffix"
+  type = string
+  default = null  
+}
+
+variable "filter_prefix" {
+  description = "s3 bucketnotification filter prefix"
+  type = string
+  default = null  
+}
+
+variable "s3_bucket_sqs_notification" {
+  description = "s3 bucket to sqs notification - true/false"
+  type = bool
+  default = false  
+}
+
+
+/*
 variable "versioning" {
   type        = bool
   default     = false
@@ -23,3 +62,4 @@ variable "bucket_prefix" {
   description = "s3 bucket prefix folder path"
   type = string
 }
+*/
