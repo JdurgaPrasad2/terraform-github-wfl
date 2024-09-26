730549a18fdc03b01e4b2212ed86483ae5bca5ce
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-26 23:55:59 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
     bucket = "s3-backend-for-testap"
-    key =    "${var.env}/test.tfstate"
-    region = var.region
+    key =    "dev/test.tfstate"
+    region = us-east-1
     encrypt = true   
     #dynamodb_table = "test-dynamodb-table"
-    dynamodb_table = "test-dynamodb-table-${var.env}"
+    dynamodb_table = "test-dynamodb-table-dev"
     }
 }
