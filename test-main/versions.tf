6bfc0a1c6fbf062fca03248cc4e4357d7508e093
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-29 16:04:27 +0530

    Create versions.tf

---
+++ test-main/versions.tf
@@ -0,0 +1,35 @@
+# versions.tf
+
+# Terraform Settings Block
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.17.0"
+    }
+  }
+  required_version = ">= 0.15"
+} 
+
+# Provider Block
+/*
+provider "aws" {
+  alias  = "dev"
+  #region = var.env[dev].region
+  region =  us-east-2
+}
+
+provider "aws" {
+  alias  = "test"
+  #region = var.env[dev].region
+  region = us-east-1
+}
+*/
+
+provider "aws" {
+  region = var.region
+  #assume_role {
+  #  role_arn = var.cross_account_role_arn
+  #}  
+}
