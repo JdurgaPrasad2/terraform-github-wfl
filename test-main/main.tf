512e68ad7f4e890955c213316c0e643c954e0264
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-29 12:06:17 +0530

    Update main.tf

--- test-main/main.tf
+++ test-main/main.tf
@@ -2,16 +2,6 @@ provider "aws" {
   region = "us-east-2"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "s3-backend-for-testap-test"
-    key =    "test/terraform.tfstate"
-    region = "us-east-2"
-    encrypt = true   
-    dynamodb_table = "dynamodb-table-test"
-    }
-}
-
 resource "aws_instance" "web" {
   ami           = "ami-037774efca2da0726"  
   instance_type = "t2.micro"
