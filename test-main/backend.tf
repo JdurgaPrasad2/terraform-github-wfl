9c94ef19b0847486bfcce0b97d3c9d7d0a661867
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-29 22:50:38 +0530

    Update backend.tf

--- test-main/backend.tf
+++ test-main/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "s3-backend-for-testap"
+    bucket = "terraform-state-s3-backend"
     key =    "test/test.tfstate"
     region = "us-east-1"
     encrypt = true   
