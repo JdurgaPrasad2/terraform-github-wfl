27cc499a3199b600d2ec78e92632f167baeeb28d
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-29 14:31:35 +0530

    Update main.tf

--- test-main/main.tf
+++ test-main/main.tf
@@ -4,5 +4,6 @@ provider "aws" {
 
 resource "aws_instance" "web" {
   ami           = "ami-037774efca2da0726"  
-  instance_type = "t2.micro"
+  #instance_type = "t2.micro"
+   instance_type = "t2.small"
 }
