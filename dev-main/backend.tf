5fe0ce208f1e7ca480168d5907869ea16a7f7f23
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-09-29 13:05:09 +0530

    Delete dev-main directory

--- dev-main/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "s3-backend-for-testap"
-    key =    "dev/dev.tfstate"
-    region = "us-east-1"
-    encrypt = true   
-    dynamodb_table = "test-dynamodb-table"
-    }
-}
