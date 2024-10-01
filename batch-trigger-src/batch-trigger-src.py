11946c8c7da9e808fd98b8dfed89d857d3f878fd
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-10-01 23:08:11 +0530

    Update batch-trigger-src.py

--- batch-trigger-src/batch-trigger-src.py
+++ batch-trigger-src/batch-trigger-src.py
@@ -1,3 +1,4 @@
+'''
 import json
 
 def lambda_handler(event, context):
@@ -6,3 +7,49 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
+'''
+
+ import json
+ import boto3
+
+ def lambda_handler(event, context):
+    
+     # TODO implement
+     batch_client = boto3.client('batch')
+     sqs = boto3.client('sqs')
+    
+     queue_url = 'https://sqs.us-east-1.amazonaws.com/663026076425/binder-batch-trigger-sqs-test'
+    
+     job_definition_name = 'tst-batch-job-def-dev'
+     job_queue_name =      'tst-batch-job-queue-dev'
+    
+     response = sqs.receive_message(
+         QueueUrl=queue_url,
+         AttributeNames=['All'],
+         MaxNumberOfMessages=10,
+         WaitTimeSeconds=10
+     )
+    
+        
+     messages = response.get('Messages', [])
+     for message in messages:
+         print(f"Received message: {message['Body']}")
+         response = batch_client.submit_job(
+                 jobName='tst-batch-job-test',
+                 jobQueue=job_queue_name,
+                 jobDefinition=job_definition_name,
+                 retryStrategy={
+                 'attempts': 1
+                 },
+         )
+        
+     for message in messages:
+         sqs.delete_message(
+             QueueUrl=queue_url,
+             ReceiptHandle=message['ReceiptHandle']
+     )
+
+     return {
+         'statusCode': 200,
+         'body': json.dumps('Hello from Lambda!')
+     }
