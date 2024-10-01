c59058e6423617aba2beb5e14ca1170c10a41c1e
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-10-02 01:31:27 +0530

    Update main.tf

--- dev-main/main.tf
+++ dev-main/main.tf
@@ -1,12 +1,14 @@
-# main.tf to call s3 bucket module
+# local varibales for main module 
 
-/*
 locals{
-  test = "${var.env == "test" ? "us-east-1" : ""}"
-  dev = "${var.env == "dev" ? "us-east-2" : ""}"
-  region = "${coalesce(local.test,local.dev)}"
+  dev        = "${var.env == "dev" ?  "us-east-1" : ""}"
+  test       = "${var.env == "test" ? "us-east-1" : ""}"
+  #qa        = "${var.env == "test" ? "us-east-1" : ""}"
+  #stage     = "${var.env == "test" ? "us-east-1" : ""}"
+  #prod      = "${var.env == "test" ? "us-east-2" : ""}"
+  region     = "${coalesce(local.dev,local.test)}"
 }
-*/
+
 
 locals {
   compute_env_name                    = "${var.project}-${var.compute_env_name}-${var.env}"
@@ -25,10 +27,12 @@ locals {
   data_ingestion_bucket_sqs_notification = var.data_ingestion_bucket_sqs_notification
 }
 
+# batch - job definition, job queue and compute environment 
+
 module "batch" {
   source                = "../modules/batch"
   project               = var.project
-  region                = var.region
+  region                = local.region
   env                   = var.env
   compute_env_name      = local.compute_env_name
   job_queue_name        = local.job_queue_name
@@ -42,10 +46,12 @@ module "batch" {
   job_def_type          = var.job_def_type
 }
 
+# lambda to trigger batch 
+
 module "lambda_batch_trigger" {
   source                  = "../modules/lambda"
   project                 = var.project
-  region                  = var.region  
+  region                  = local.region 
   env                     = var.env
   function_name           = local.batch_trigger_function_name
   source_dir              = var.batch_trigger_src_dir
@@ -57,11 +63,13 @@ module "lambda_batch_trigger" {
   job_name                = local.job_name
 }
 
+# eventbridge rule to invoke lambda which triggers batch
+
 module "eventbridge_rule_batch_trigger" {
   depends_on = [module.lambda_batch_trigger]
   source                  = "../modules/eventbridge"
   project                 = var.project
-  region                  = var.region  
+  region                  = local.region  
   env                     = var.env
   function_name           = local.batch_trigger_function_name
   function_arn           =  module.lambda_batch_trigger.lambda_arn
@@ -70,19 +78,23 @@ module "eventbridge_rule_batch_trigger" {
   event_schedule          = local.batch_trigger_event_schedule
 }
 
+# ingestion sqs queue  
+
 module "ingestion_sqs_queue" {
   source                      = "../modules/sqs"
   project                     = var.project
-  region                      = var.region  
+  region                      = local.region  
   env                         = var.env
   sqs_queue_name              = local.ingestion_sqs_queue_name
   #bucket_arn                  = module.data_ingestion_bucket.s3_bucket_arn
 }
 
+# ingestion s3 bucket
+
 module "data_ingestion_bucket" {
   source                      = "../modules/s3"
   project                     = var.project
-  region                      = var.region  
+  region                      = local.region
   env                         = var.env
   bucket_name                 = local.data_ingestion_bucket_name
   sqs_queue_arn               = module.ingestion_sqs_queue.sqs_queue_arn
@@ -118,3 +130,49 @@ resource "aws_sqs_queue_policy" "queue_policy" {
   queue_url             = module.ingestion_sqs_queue.sqs_queue_url
   policy                = local.queue_access_policy
 }
+
+
+# lambda to trigger ingestion 
+
+locals {
+  ingestion_trigger_function_name         = "${var.project}-${var.ingestion_trigger_function_name}-${var.env}"
+  ingestion_trigger_event_rule_name       = "${var.project}-${var.ingestion_trigger_event_rule_name}-${var.env}"
+  ingestion_trigger_event_rule_desc       = "ingestion trigger event rule name"
+  ingestion_trigger_event_schedule        = "${var.ingestion_trigger_event_schedule}"
+}
+
+
+module "lambda_ingestion_trigger" {
+  source                  = "../modules/lambda"
+  project                 = var.project
+  region                  = local.region 
+  env                     = var.env
+  function_name           = local.ingestion_trigger_function_name 
+  source_dir              = var.ingestion_trigger_src_dir
+  output_path             = var.ingestion_trigger_src_op_path
+  job_queue_name          = local.job_queue_name
+  job_def_name            = local.job_def_name
+  handler                 = "${var.ingestion_trigger_src_op_path}.lambda_handler"
+  runtime                 = var.runtime
+  job_name                = local.job_name
+}
+
+# eventbridge rule to invoke lambda which triggers batch
+
+module "eventbridge_rule_batch_trigger" {
+  depends_on = [module.lambda_batch_trigger]
+  source                  = "../modules/eventbridge"
+  project                 = var.project
+  region                  = local.region  
+  env                     = var.env
+  function_name           = local.batch_trigger_function_name
+  function_arn           =  module.lambda_batch_trigger.lambda_arn
+  event_rule_name         = local.batch_trigger_event_rule_name
+  event_rule_desc         = local.batch_trigger_event_rule_desc
+  event_schedule          = local.batch_trigger_event_schedule
+}
+
+
+
+
+
