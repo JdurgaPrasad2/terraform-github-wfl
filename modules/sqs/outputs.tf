ddfc6e4a9ace6ac5b53dcb12a8d589ab74868a09
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-10-01 20:20:27 +0530

    Update outputs.tf

--- modules/sqs/outputs.tf
+++ modules/sqs/outputs.tf
@@ -1,10 +1,10 @@
 # outputs.tf
-output "s3_queue_arn" {
+output "sqs_queue_arn" {
   description = "Generated sqs queue arn"
   value       = aws_sqs_queue.queue.arn
 }
 
-output "s3_queue_url" {
+output "sqs_queue_url" {
   description = "Generated sqs queue url"
   value       = aws_sqs_queue.queue.id
 }
