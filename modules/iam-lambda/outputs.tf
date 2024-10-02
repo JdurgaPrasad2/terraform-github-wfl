d963db2f51e2779d9e8778e5273305b275b18b8f
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-10-02 22:19:22 +0530

    Update outputs.tf

--- modules/iam-lambda/outputs.tf
+++ modules/iam-lambda/outputs.tf
@@ -1,10 +1,10 @@
 # outputs.tf
-output "iam_role_arn" {
+output "role_arn" {
   description = "Generated iam role arn"
   value       = aws_iam_role.lambda_execution.arn
 }
 
-output "iam_role_id" {
+output "role_id" {
   description = "Generated iam role name"
   value       = aws_iam_role.lambda_execution.id
 }
