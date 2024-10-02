686516cde2bbe2ac2a44a2c71f12dcb7c7decae1
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-10-03 02:02:07 +0530

    Update dynamodb-table.tf

--- modules/dynamodb/dynamodb-table.tf
+++ modules/dynamodb/dynamodb-table.tf
@@ -36,8 +36,8 @@ resource "aws_dynamodb_table" "status_table" {
     name               = "Doc-Id-Index"
     hash_key           = "Doc-Id"
     range_key          = "Doc-Name"
-    #write_capacity     = ""
-    #read_capacity      = ""
+    write_capacity     = "1000"
+    read_capacity      = "3000"
     projection_type    = "INCLUDE"
     non_key_attributes = ["Doc-Id"]
   } 
