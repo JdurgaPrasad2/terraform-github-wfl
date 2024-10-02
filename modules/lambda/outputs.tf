b2d841f02a6e3f112a705e42beae23245531beaa
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-10-02 22:00:45 +0530

    Update outputs.tf

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -3,3 +3,8 @@ output "lambda_arn" {
   description = "Generated lambda arn"
   value       = aws_lambda_function.function.arn
 }
+
+output "lambda_id" {
+  description = "Generated lambda fucntion id"
+  value       = aws_lambda_function.function.id
+}
