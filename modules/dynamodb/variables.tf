8f99104bb1077a6a4ae6c9703391ec33543985b8
JdurgaPrasad2 <70485767+JdurgaPrasad2@users.noreply.github.com>
2024-10-03 00:46:33 +0530

    Create variables.tf

---
+++ modules/dynamodb/variables.tf
@@ -0,0 +1,36 @@
+# variables.tf
+
+variable "env" {
+  description = "Target environment for the deployment"
+  type        = string
+}
+
+variable "region" {
+  description = "Target region for the deployment"
+  type        = string
+}
+
+variable "project" {
+  description = "Department requesting the resource"
+  type        = string
+}
+
+variable "lambda_execution_role_name" {
+  description = "lambda execution role name"
+  type        = string
+}
+
+variable "lambda_function_id" {
+  description = "lambda function id"
+  type        = string
+}
+
+variable "lambda_execution_policy_name" {
+  description = "lambda execution policy name"
+  type        = string
+}
+
+variable "managed_policy_list_for_lambda" {
+  description = "iam managed policy list for lambda"
+  type        = list(string)
+}
